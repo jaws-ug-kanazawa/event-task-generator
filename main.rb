b692556368e59a6f49730f3b466f51937f77de54
PharaohKJ <kato@phalanxware.com>
2022-04-07 11:51:53 +0900

    upate gems. Expert Online 用テンプレート作成。

--- main.rb
+++ main.rb
@@ -2,24 +2,27 @@
 require 'octokit'
 require 'dotenv'
 require 'yaml'
+require 'erb'
 
 Dotenv.load
 
-exit if ARGV.empty?
+if ARGV.empty?
+  puts 'ruby main.rb <YAML-FILE> <EventTitle>'
+  exit
+end
 
-yml = YAML.load_file(ARGV[0])
+erb_str = File.read(ARGV[0])
+yaml = ERB.new(erb_str).result_with_hash(title: ARGV[1])
+
+yml = YAML.safe_load(yaml)
 
 client = Octokit::Client.new(
   login:    ENV['GITHUB_USER'],
   password: ENV['GITHUB_TOKEN']
 )
-
 repository = yml['repository']
 eventname  = yml['event']
 yml['tasks'].each do |t|
-  p client.create_issue(
-    repository,
-    "#{eventname} - #{t['title']}",
-    t['body']
-  )
+  # p "#{repository} #{eventname} - #{t['title']}"
+  p client.create_issue(repository, "#{eventname} - #{t['title']}", t['body'])
 end
